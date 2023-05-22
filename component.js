a0c99cd7ba1e2966f68788abe90b8c72fa755e5a
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-22 20:28:51 +0200

    Done

---
+++ component.js
@@ -0,0 +1,32 @@
+export class Component {
+  constructor(options) {
+    this.document = options.shadowDom;
+    this.arg1 = this.document.getElementById("arg1");
+    this.initPromise = this.init();
+  }
+
+  async init() {
+    await uiBuilder.ready(this.arg1);
+    await this.arg1.component.inputDefinition({
+      type: "unify",
+      name: "Not",
+    });
+  }
+
+  setConfig(config) {
+    this.config = config;
+  }
+
+  async setValue(value) {
+    await this.initPromise;
+    await this.arg1.component.setValue(value.value);
+  }
+
+  async getValue() {
+    return {
+      evaluate: true,
+      value: await this.arg1.component.getValue(),
+      type: "not",
+    };
+  }
+}
---
+++ handler.js
@@ -0,0 +1,6 @@
+import { resolveInput } from "../../public/api.js";
+import config from "@proxtx/config";
+
+export const evaluate = async (value) => {
+  return !(await resolveInput(config.pwd, value.code));
+};
