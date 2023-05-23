cd6914507636828478653560c4a8b986f00b2150
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-05-23 18:33:43 +0200

    fix wrong var being evaluated

--- handler.js
+++ handler.js
@@ -2,5 +2,5 @@ import { resolveInput } from "../../public/api.js";
 import config from "@proxtx/config";
 
 export const evaluate = async (value) => {
-  return !(await resolveInput(config.pwd, value.code));
+  return !(await resolveInput(config.pwd, value.value));
 };
